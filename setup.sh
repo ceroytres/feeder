cc6b2fd7bcdfdf7dc5cf9bdacbd0e98feb3696a3
Diego <dmarez8ece@gmail.com>
2020-07-17 13:44:58 -0700

    fix scripts

--- setup.sh
+++ setup.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-apt-get update && sudo apt-get install ffmpeg build-essential cmake \
+sudo apt-get update && sudo apt-get install -y ffmpeg build-essential cmake \
                                   libopenblas-dev liblapack-dev \
                                   libx11-dev libgtk-3-dev 
 
 cd v4l2loopback && make && sudo make install
 
+cd ..
 pip install -r ./requirements.txt
